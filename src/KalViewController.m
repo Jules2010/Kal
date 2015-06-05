2ec90454dc6b21c1db69721fe4431a24a199a9f0
Marcos Piovezan <marcos.piovezan@exxonmobil.com>
2015-06-05 14:31:05 -0500

    re-calculate screen size on rotation

--- src/KalViewController.m
+++ src/KalViewController.m
@@ -220,6 +220,11 @@ NSString *const KalDataSourceChangedNotification = @"KalDataSourceChangedNotific
   [tableView flashScrollIndicators];
 }
 
+-(void)didRotateFromInterfaceOrientation:(UIInterfaceOrientation)fromInterfaceOrientation
+{
+    [self loadView];
+}
+
 #pragma mark -
 
 - (void)dealloc
